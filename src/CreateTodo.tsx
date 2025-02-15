6e225b648b91675c38da2a0361f1a09904b1f0a0
youngwon Kim <youngwonkim@youngwonui-MacBookPro.local>
2025-02-15 12:28:07 -0500

    addfeature

--- src/CreateTodo.tsx
+++ src/CreateTodo.tsx
@@ -1,9 +1,30 @@
-export default function CreateTodo() {
+interface InputTextProps {
+    onChange(e: React.ChangeEvent<HTMLInputElement>): void;
+    onSubmit(event: React.FormEvent<HTMLFormElement>): void;
+    inputText: string;
+}
+
+
+
+export default function CreateTodo({
+    onChange,
+    onSubmit,
+    inputText
+}: InputTextProps) {
     return (
       <div className="todoCreateContainer">
-        <form>
+        {/* <form>
           <input type="text" placeholder="할 일을 입력해 주세요." />
           <button>등록하기</button>
+        </form> */}
+        <form onSubmit={(event) => onSubmit(event)}>
+            <input
+            onChange={(e) => onChange(e)}
+            type="text"
+            placeholder="할 일을 입력해 주세요."
+            value={inputText}
+            />
+            <button type="submit">등록하기</button>
         </form>
       </div>
     );
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -11,16 +11,49 @@ interface TList {
 
 export default function TodoList() {
 
+
+    const [inputText, setInputText] = useState("");
+
     const [todoList, setTodoList] = useState<TList[]>([
-        { id: 1,
-          text: "할일1",
-          completed: false
-        },
-        { id: 2,
-          text: "할일2",
-          completed: false
-        }
+        // { id: 1,
+        //   text: "할일1",
+        //   completed: false
+        // },
+        // { id: 2,
+        //   text: "할일2",
+        //   completed: false
+        // }
     ]);
+
+
+    // 입력값 변경내용 확인
+    const textTypingHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputText(e.target.value);
+    };
+
+    // 입력 확인
+    const textInputHandler = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        // 폼을 제출할 때, 폼을 서버로 전송하는 기본 동작을 막고, 그 대신 JavaScript에서 다른 처리를 하도록 할 수 있습니다.
+        const newTodo: TList = {
+        id: Date.now(),
+        text: inputText,
+        completed: false,
+        };
+        setTodoList([...todoList, newTodo]);
+        // 1. todoList 배열의 모든 항목을 복사한 후 (...todoList).
+        // 2. newTodo를 배열의 끝에 추가하여 새로운 배열을 만듭니다.
+        // 3. setTodoList를 호출하여, 새로운 배열로 todoList 상태를 갱신합니다.
+
+
+
+        // 아래처럼도 사용 가능
+        // setTodoList(todoList.concat(newTodo));
+        // 입력한 값 지우기
+        setInputText("");
+    };
+
+
 
 
     return (
@@ -28,7 +61,11 @@ export default function TodoList() {
           {todoList.map((item) => (
             <TodoItem key={item.id} text={item.text} completed={item.completed} />
           ))}
-          <CreateTodo />
+          <CreateTodo
+            onChange={textTypingHandler}
+            onSubmit={textInputHandler}
+            inputText={inputText}
+          />
         
         </div>
     );
